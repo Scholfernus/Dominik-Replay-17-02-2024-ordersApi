263314b2b58c4a4390389d40238d79a9cca9b552
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-20 22:32:39 +0100

    Zmiany do poprawnie działającej aplikacji - properties i data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO order_model(order_number, status, item ) VALUES ('258LK55','Przyjęte','Koc');
+INSERT INTO order_model(order_number, status, item ) VALUES ('OK8549PP','Wysłane','Bike');
+INSERT INTO order_model(order_number, status, item ) VALUES ('LPO951847I','W trakcie realizacji','Boat');
+INSERT INTO order_model(order_number, status, item ) VALUES ('PPK456789MN','Zakończone','Car');
+INSERT INTO order_model(order_number, status, item ) VALUES ('SH89526PA','W trakcie realizacji','Ship');
+INSERT INTO order_model(order_number, status, item ) VALUES ('T753869FG','Zakończone','Towel');
